f88faf68f7b13d40d3dd7540c2667fbcbac4cb6e
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 21:12:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++ ) {
     if (array[i] === stopValue) {
       break;
-    } else {
-      changeValue(array[i])
     }
+    changeValue(array[i])
     return array;
   }
 }

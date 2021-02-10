685d3feefaffe73ca602b473307ae43ac24eee0e
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 21:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,12 @@ function keepGoing(array, changeValue, skipValue) {
   return array;
 }
 
-function findBy(array, findFn) {}
+function findBy(array, findFn) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === findFn) {
+      break;
+    } else {
+      return null;
+    }
+  }
+}
